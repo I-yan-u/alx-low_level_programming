5add27b1fa77f5a23b6f985ed3e4524d9ea51ecc
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-12 15:16:10 -0700

    Task 1 - 6 and adv task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Body of code
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*A program that output with the printf method*/
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
