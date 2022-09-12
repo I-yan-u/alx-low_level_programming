7be69a4b05753f53cf66cb000c4103f296dbc2be
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-12 15:28:20 -0700

    adv tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Body of code
+ *
+ * Return: Always 1 (successful)
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (0);
+}
