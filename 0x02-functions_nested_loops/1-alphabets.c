4f2bbcaf9424332dc084d4817a496a875c35a3d1
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-13 16:12:26 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabets.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - To print out alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+char alpha;
+alpha = 'a';
+
+for (alpha <= 'z'; alpha++;)
+{
+_putchar(alpha);
+}
+}
