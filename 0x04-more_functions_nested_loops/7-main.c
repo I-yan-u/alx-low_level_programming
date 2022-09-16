d007b31d210209e73ed351810051aea51e559208
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-16 15:34:48 -0700

    Diagonals

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
+
+
 /**
+
  * main - check the code
+
  *
+
  * Return: Always 0.
+
  */
 
 int main(void)
   
 {
   
-  print_line(0);
+  print_diagonal(0);
   
-  print_line(2);
+  print_diagonal(2);
   
-  print_line(10);
+  print_diagonal(10);
   
-  print_line(-4);
+  print_diagonal(-4);
   
   return (0);
   
