271126b12e1b4cd8af1edb9ce06bf5423f02cc1f
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-16 16:34:55 -0700

    Triagles

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -16,13 +16,13 @@ int main(void)
   
 {
   
-  print_diagonal(0);
+  print_triangle(2);
   
-  print_diagonal(2);
+  print_triangle(10);
   
-  print_diagonal(10);
+  print_triangle(1);
   
-  print_diagonal(-4);
+  print_triangle(0);
   
   return (0);
   
