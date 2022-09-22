e287428df51d0534874a95ce6ebb969df9dc7b66
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-22 10:56:21 -0700

    Str_concatenator

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,26 @@
-int _putchar(char c);
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+
+ *void prototypes()
+
+ *int prototypes()
+
+ *char prototypes()
+
+ */
+
+void reverse_array(int *a, int n);
+
+void print_number(int n);
+
+void print_buffer(char *b, int size);
+
+int _strcmp(char *s1, char *s2);
 
 char *_strcat(char *dest, char *src);
 
@@ -6,12 +28,18 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
-int _strcmp(char *s1, char *s2);
-
-void reverse_array(int *a, int n);
-
 char *string_toupper(char *);
 
 char *cap_string(char *);
 
 char *leet(char *);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
+
+
+#endif /*MAIN_H*/
