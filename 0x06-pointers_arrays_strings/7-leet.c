4ffca2a87e932b7ce4337d079198d53e00cf14ae
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-24 09:57:37 -0700

    1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,9 +13,9 @@ int i, j;
 char a[] = "aAeEoOtTlL";
 char b[] = "4433007711";
 
-for (i = 0, *(s + i); i++)
+for (i = 0; *(s + i); i++)
 {
-for (j = 0; j <= 9)
+for (j = 0; j <= 9; j++)
 {
 if (a[j] == *(s + i))
 {
