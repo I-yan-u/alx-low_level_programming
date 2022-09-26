89cdabc1a258c0db81800591a1fea93d7a7f030b
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-26 08:08:06 -0700

    set memories

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: The pointer to the array.
+ * @b: The constant byte.
+ * @n: number of times to fill the array.
+ *
+ * Return: The pointer to the array.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+return (s);
+}
