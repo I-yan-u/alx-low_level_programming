74a0531a4b89dad3a1ea46f1bb390a6d6583a2a5
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-26 21:53:57 -0700

    Sting checker

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: source
+ * Return: number of bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+unsigned int prev = 0;
+inti, j;
+
+i = 0;
+j = 0;
+
+while (*(s + i) != '\0')
+{ 
+j = 0;
+prev = count;
+while (*(accept + j) != '\0')
+{
+if (*(s + i) == *(accept + j))
+{
+count++;
+}
+j++;
+}
+if (prev == count)
+{
+break;
+}
+i++;
+}
+return (count);
+}
