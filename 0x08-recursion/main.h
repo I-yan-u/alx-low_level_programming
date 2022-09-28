fff0b6ab134b4fe0ebc0c2e50f682efa7fe4f0ce
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-28 16:05:44 -0700

    Puts

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@
 * int prototypes()
 */
 
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -16,5 +17,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+
 
 #endif /*MAIN_H*/
