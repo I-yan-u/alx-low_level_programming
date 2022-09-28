552f414ce33e7eb3f927389ca4157ded84fa3719
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-28 16:00:25 -0700

    Puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - send character to stdout
+ * @s: the string to be checked
+ *
+ *
+ * Return: tthe string
+ */
+
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	putchar(*(s + 0));
+	_puts_recursion((s + 1));
+	}
+	else
+	{
+		putchar('\n');
+		return;
+	}
+}
+
