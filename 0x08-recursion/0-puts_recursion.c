6c0eea15237c704c80ab1a8338a14025a6d82afb
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-28 16:04:23 -0700

    Puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,12 +14,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-	putchar(*(s + 0));
+	_putchar(*(s + 0));
 	_puts_recursion((s + 1));
 	}
 	else
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
 }
