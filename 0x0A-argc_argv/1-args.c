c9266a39835369ce00b07cbd7a7c077273c1abd6
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-30 15:39:02 -0700

    Argument scores

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,12 +12,9 @@
 
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-	if (argc > 1)
-	{
-		printf("%d\n", argc);
-	}
-	else
-		printf("%d\n", argc - 1);
+	int i = argc - 1;
+
+	printf("%d\n", i);
 	return (0);
 }
 
