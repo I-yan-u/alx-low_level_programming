97c2490695fde1214ec0baee1a79f83f9142cae4
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-30 15:25:19 -0700

    Argument scores

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of args passed to the prog
+ *
+ * @argc: The number of args passed to the programs
+ * @argv: The array of args
+ *
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
