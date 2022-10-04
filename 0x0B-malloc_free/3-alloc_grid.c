452743eba5d5a2583b29ffdca0c8cd2f7bcc59be
I-yan-u <iyanuajimobi12@gmail.com>
2022-10-04 15:55:31 -0700

    powerGrid creator

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: The 2D grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, **grid;
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+	grid = (int **) malloc(width * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = (int *) malloc(height * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			while (i <= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	return (grid);
+
+}
+
