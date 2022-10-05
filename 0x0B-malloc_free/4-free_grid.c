fc0b3822bb3a5e0c0899e2e8293180687466f5a1
I-yan-u <iyanuajimobi12@gmail.com>
2022-10-05 09:21:33 -0700

    Freedom

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+ 		free(grid[i]);
+	}
+	free(grid);
+}
+
