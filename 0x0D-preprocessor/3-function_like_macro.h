1f09f30a2afe9bd0a6939929d1d184cda6b24d4d
I-yan-u <iyanuajimobi12@gmail.com>
2022-10-10 14:18:20 -0700

    macroFunction

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#define ABS(c) ((c) > (-1) ? (c) : (c * -1))
+#define ABS(c) ((c) > (-1) ? (c) : ((c) * (-1)))
 #endif /* !FUNCTION_LIKE_MACRO_H */
 
