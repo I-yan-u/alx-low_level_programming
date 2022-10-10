c24ee1a3455df6409e01d9fd39fcfde28982e18f
I-yan-u <iyanuajimobi12@gmail.com>
2022-10-10 15:49:26 -0700

    Initialize Dawgs!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,15 @@
  * a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* !DOG_H */
