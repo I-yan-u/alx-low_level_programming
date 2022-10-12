a4673e45128a6fc03f19c6eccc1174e0fd57ee65
I-yan-u <iyanuajimobi12@gmail.com>
2022-10-12 12:06:23 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Body of code
+ * main - body of code
  *
- * Return: Always 1 (successful)
+ * Return: Always 0 (sucess)
  */
 int main(void)
 {
-write(2,
-"and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, " and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
+
