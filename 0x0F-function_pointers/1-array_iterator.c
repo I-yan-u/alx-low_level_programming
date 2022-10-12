6bc77b7673c8dfd9c874ddcf6d1d243681a40373
I-yan-u <iyanuajimobi12@gmail.com>
2022-10-12 13:28:49 -0700

    iterate

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints content of an array with defined options
+ * @array: the array
+ * @size: size of array
+ * @action: the option
+ *
+ * Return: none
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (array == NULL || size == 0)
+		return;
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+
