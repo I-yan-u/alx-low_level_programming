46ed23df30d70bcfd38ceef11b9dcfb15741c31a
Iyanu <iyanuajimobi12@gmail.com>
2022-10-15 16:26:46 -0700

    Print errting

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct fmt - A struct consisting of a char and *f
+ * @c: character
+ * @f: pointer to function
+ */
+typedef struct fmt
+{
+	char c;
+	void (*f)(va_list);
+}fmt;
+
 int _putchar(int c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
