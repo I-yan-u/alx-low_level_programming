a61ffb758b6ab06fa04e50a7b7ed0bced4687fdd
Iyanu <iyanuajimobi12@gmail.com>
2022-10-23 02:47:40 -0700

    faster than main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,14 @@
 #include "lists.h"
 
+void __attribute__ ((constructor)) beforeMain();
+
 /**
  * bmain - prints text before main
  *
  * Return: void
  */
 
-void __attribute__ ((constructor)) bmain()
+void beforeMain(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
