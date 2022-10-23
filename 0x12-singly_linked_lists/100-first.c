f8f4356f3ce53349e7f114c40571df510d317a3e
Iyanu <iyanuajimobi12@gmail.com>
2022-10-23 01:28:02 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * bmain - prints text before main
+ *
+ * Return: void
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
+
