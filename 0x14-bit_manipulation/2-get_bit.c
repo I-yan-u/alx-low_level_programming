5b0cbedb88021563da52384cbee095fb24121329
Iyanu <iyanuajimobi12@gmail.com>
2022-10-30 09:16:47 -0700

    gimme the 1's and 0's

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer
+ * @index: index
+ * Return: integer
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	return ((n >> index) & 1);
+}
+
