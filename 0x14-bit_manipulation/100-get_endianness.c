c050a697bdd64e5f5cdf8f6a2626bef020b083f8
Iyanu <iyanuajimobi12@gmail.com>
2022-10-30 15:24:45 -0700

    Get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - sets the value of a bit to 1 at a given index
+ * Return: 1 if little endian 0 if big endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *j = (char *)&i;
+
+	if (*j)
+		return (1);
+	return (0);
+}
+
