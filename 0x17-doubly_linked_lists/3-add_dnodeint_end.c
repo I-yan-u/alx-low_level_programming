5d6ac73eaa812d50a37f6c8a3b44fd2cc77515ad
Iyanu <iyanuajimobi12@gmail.com>
2022-12-15 15:04:03 -0800

    made betty corrections

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - To add node at beginning of list.
+ * add_dnodeint_end - To add node at beginning of list.
  * @head: The pointer to the head of the list.
  * @n: The node data (int).
  *
@@ -10,26 +10,26 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new, *temp;
+	dlistint_t *new, *temp;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->prev = NULL;
+	new->n = n;
+	new->prev = NULL;
 
-    if (*head == NULL)
-    {
-        new->next = NULL;
-        *head = new;
-        return (new);
-    }
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    temp->next = new;
-    new->prev = temp;
-    return (new);
+	temp->next = new;
+	new->prev = temp;
+	return (new);
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,13 +1,20 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - Frees the memory allocated to create the list
+ * @head: pointer to head.
+ *
+ * Return: void
+ */
+
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
-    
-    while (head)
-    {
-	temp = head->next;
-        free(head);
-	head = temp;
-    }
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
