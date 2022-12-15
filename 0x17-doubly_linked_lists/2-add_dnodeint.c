060be264021008a426a518ba8bc3d84ebf8284c4
Iyanu <iyanuajimobi12@gmail.com>
2022-12-15 15:11:49 -0800

    Added * to head on return

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,23 +10,23 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
+	dlistint_t *new;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->prev = NULL;
+	new->n = n;
+	new->prev = NULL;
 
-    if (*head == NULL)
-    {
-        new->next = NULL;
-        *head = new;
-        return (*head);
-    }
-    new->next = *head;
-    (*head)->prev = new;
-    *head = new;
-    return (*head);
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
+	return (*head);
 }
