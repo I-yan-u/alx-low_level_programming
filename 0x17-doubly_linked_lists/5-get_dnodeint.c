95b336bfac3b7448d7483ac73c62b02ef29dc4ff
Iyanu <iyanuajimobi12@gmail.com>
2022-12-15 15:48:23 -0800

    Node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Gets node at selected position(index)
+ * @head: The pointer to first nde
+ *
+ * Return: Node at index.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t count = 0;
+	dlistint_t *temp;
+
+	if (!head)
+		return (NULL);
+
+	temp = head;
+	while (temp)
+	{
+		if (index == count)
+			return (temp);
+		temp = temp->next;
+		count++;
+	}
+	return (NULL);
+}
