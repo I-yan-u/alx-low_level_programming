e7fd61244f103a3ce90a54bc98a2303047482298
Iyanu <iyanuajimobi12@gmail.com>
2023-01-06 15:32:39 -0800

    - Hash table\n - Hash algo (djb2)\n - Key_index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Function to create an index for a key in an Hash table
+ *
+ * @key: The key to create an index for
+ * @size: The size of the Hash table
+ *
+ * Return: It returns an index on the Hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) % size);
+}
