69b76abc8dd6f7746634834257a1d222e143e3b8
Iyanu <iyanuajimobi12@gmail.com>
2023-01-06 14:47:55 -0800

    correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
