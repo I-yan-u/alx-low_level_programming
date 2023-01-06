b65de23389645a6c1e43621d8aab40b06902547d
Iyanu <iyanuajimobi12@gmail.com>
2023-01-06 15:12:35 -0800

    djb2 hash algo

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,13 +8,13 @@
  * Return: the calculated hash
  */
 
-unsigned long int hash_djb2(const unsigned char *string)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
 
 	hash = 5381;
-	while (c = *str++)
+	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
