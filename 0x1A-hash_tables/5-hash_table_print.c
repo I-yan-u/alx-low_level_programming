e3e88a1ed91cdbdf94e142ef959a16c03520266c
Iyanu <iyanuajimobi12@gmail.com>
2023-01-10 15:07:42 -0800

    print or delete hashtables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,33 +2,33 @@
 /**
  * hash_table_print - Prints all the (none-empty) content of an hash table
  * @ht: The hash table
- * 
+ *
 */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int index = 0;
-    hash_node_t *point;
-    int first = 1;
+	unsigned long int index = 0;
+	hash_node_t *point;
+	int first = 1;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+	return;
 
-    printf("{");
-    while (index < ht->size)
-    {
-        point = ht->array[index];
-        while (point != NULL)
-        {
-            if (first == 1)
-            {
-                printf("'%s': '%s'", point->key, point->value);
-                first = 0;
-            }
-            else
-                printf(", '%s': '%s'", point->key, point->value);
-            point = point->next;
-        }
-        index++;
-    }
-    printf("}\n");
+	printf("{");
+	while (index < ht->size)
+	{
+		point = ht->array[index];
+		while (point != NULL)
+		{
+			if (first == 1)
+			{
+				printf("'%s': '%s'", point->key, point->value);
+				first = 0;
+			}
+			else
+				printf(", '%s': '%s'", point->key, point->value);
+			point = point->next;
+		}
+		index++;
+	}
+	printf("}\n");
 }
